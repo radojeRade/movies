82239a552ff6b75012ef2a8452ba315fb891c3d2
radojeRade <aleksandravidic88@gmail.com>
2022-10-03 21:20:45 +0200

    Zadatak 2

---
+++ database/migrations/2022_10_03_190850_create_movies.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('genre');
+            $table->string('director');
+            $table->integer('year');
+            $table->text('story_line');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
